4c73707be900ddb833a148a112c7b9b0127f1477
Paula Alves <paulafernandaalves@icloud.com>
2019-11-18 18:01:05 -0200

    Add project "my site"
    Add project "my site" from django project website

---
+++ mysite/.mysite/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/user/paulaalves/git/django_projects/mysite/.mysite/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
